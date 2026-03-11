112b2c4af3e7a01771078c1a5f8f1a17af632d5d
Campbell Swan <147725350+CSwan300@users.noreply.github.com>
2026-03-11 15:56:24 +0000

    Revise personal bio for clarity and detail
    Updated bio to reflect current interests and goals.

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -5,5 +5,5 @@ export const personalInfo = {
     github: "https://github.com/CSwan300",
     linkedin: "https://www.linkedin.com/in/campbell-swan/",
     resumeFile: "CampbellSwanResume.pdf",
-    bio: "I'm a Computer Science student based in Scotland with a passion for technology. I enjoy exploring various programming languages. My interests span across full-stack development, data analysis or really anything i find interesting!.",
+    bio: "I'm a Computer Science student based in Scotland with a genuine passion for technology. I enjoy exploring different programming languages and working on full‑stack and data‑focused projects. I’m currently looking for internships or placement opportunities where I can contribute to real-world software while continuing to learn and grow!",
 };
