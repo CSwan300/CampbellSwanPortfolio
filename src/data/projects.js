46520be16e3a8dde9a1a1cd23c1ece007bef06d0
Campbell Swan <147725350+CSwan300@users.noreply.github.com>
2026-04-19 23:04:38 +0100

    Refactor project entries in projects.js
    Updated project IDs and descriptions in projects.js.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,6 @@
 export const projects = [
     {
-        id: 2,
+        id: 1,
         title: "Yoga Booking Site",
         description: "A Fullstack WebApp Created for a university module based on a yoga booking site - note deployed on render free tier so takes about 50 seconds to warm up ",
         technologies: ["Javascript", "Mustache.js", "css", "Node.js","Express.js","JWT","CI/CD","Expanded MVC"],
@@ -8,33 +8,22 @@ export const projects = [
         demo: "https://campbellswanwebdev2courseworklivedemo.onrender.com/",
         image: "./images/Yoga.png",
     },
-
-
     {
-        id: 3,
-        title: "Language Explorer",
-        description: "Collection of projects exploring various  programming languages and their ecosystems",
-        technologies: ["Kotlin", "Scala", "Java", "Groovy","C","Ruby","C#"],
-        github: "https://github.com/CSwan300/Random-Projects",
-        // no demo here
-        image: "./images/placeholder.png",
-    },
-    {
-        id: 4,
+        id: 2,
         title: "Portfolio Website",
-        description: "portfolio showcasing projects and skills with a responsive design hint your on it haha",
-        technologies: ["React", "Tailwind CSS", "JavaScript","Vite"],
+        description: "portfolio showcasing projects and skills with a responsive design created adapted from a template and with the assistance of claude hosted on vercel",
+        technologies: ["React", "Tailwind CSS", "JavaScript","Vite","LLM"],
         github: "https://github.com/CSwan300/CampbellSwanPortfolio",
         demo: "https://campbellswan.vercel.app/",
         image: "./images/website.png",
     },
     {
-        id: 6,
+        id: 3,
         title: "IEUK 2025",
         description: "a comprehensive web server log analysis tool that automates download, performs detailed technical analysis including bot detection and suspicious activity identification, and presents results via rich visualizations and reports." ,
         technologies: ["Python", "Docker", "CI/CD"],
         github: "https://github.com/CSwan300/PythonDataProjectTask",
-        // demo: "#",
+        // no demo
         image: "./images/log_analysis_report.png",
     },
 ];
